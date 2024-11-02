d68388622bec6d8d564b15eb1be344dc84a997c5
Oleksandr Panchyk <65217120+opanchyk@users.noreply.github.com>
2024-11-02 20:01:54 +0200

    add vars

--- week4/game.py
+++ week4/game.py
@@ -1,6 +1,9 @@
 import random
 word_list = ["cat", "wolf", "monkey", "cow"]
 
+end_game =  False
+lives = 6
+
 word = random.choice(word_list)
 print (word)
 display = []
@@ -11,17 +14,27 @@ for _ in range (lengt_word):
     display += "_"
 print (display)
 
-input_letter = input ("Input letter. - ")
-print (input_letter)
+while not end_game:
+    input_letter = input ("Input letter. - ")
+    print (input_letter)
 
-for position in range (lengt_word):
-    letter = word[position]
-    # print (f"position = {position} , letter = {letter}")
-    if letter == input_letter:
-        display[position] = letter
+    for position in range (lengt_word):
+        letter = word[position]
+        # print (f"position = {position} , letter = {letter}")
+        if letter == input_letter:
+            display[position] = letter
 
-if input_letter not in word:
-    print ("You lose")
+    if input_letter not in word:
+        #lives -=1
+        lives = lives -1
+        
+        if lives == 0:
+            end_game = True
+            print("You lose")
+            
+    print(f"{' '.join(display)}")
 
-print(f"{' '.join(display)}")
+    if '_' not in display:
+        end_game = True
+        print ("You win")
 
