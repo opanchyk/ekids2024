aef1cddbc04d69aae95a9c9c727c73756146587a
Oleksandr Panchyk <65217120+opanchyk@users.noreply.github.com>
2025-04-05 10:07:16 +0300

    Update game.py

--- week4/game.py
+++ week4/game.py
@@ -1,46 +1,28 @@
 import os
 import random
-from  hangman_stages import stages
+from hangman_stages import stages
 
-word_list = ["cat", "wolf", "monkey", "cow"]
+word_list=["cat","monkey","dog"]
 
-end_game =  False
+end_game = False
 lives = 6
 
 word = random.choice(word_list)
-print (word)
-display = []
-lengt_word = len(word)
-print (lengt_word)
 
-for _ in range (lengt_word):
+print (word)
+
+display = []
+length_word = len(word)
+print(length_word)
+
+
+
+
+for _ in range (length_word):
     display += "_"
+
 print (display)
 
 while not end_game:
-    input_letter = input ("Input letter. - ")
-    os.system ('cls') # Windows
-    # os.system('clear')  # Linux MacOS
-    print (input_letter)
-
-    for position in range (lengt_word):
-        letter = word[position]
-        # print (f"position = {position} , letter = {letter}")
-        if letter == input_letter:
-            display[position] = letter
-
-    if input_letter not in word:
-        #lives -=1
-        lives = lives -1
-
-        if lives == 0:
-            end_game = True
-            print("You lose")
-
-    print(f"{' '.join(display)}")
-
-    if '_' not in display:
-        end_game = True
-        print ("You win")
-    print (stages [lives])
-
+    input_letter = input("Input letter. - ")
+    print(input_letter)
